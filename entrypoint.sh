8ea03e337242f017a679ad39949d885f315a7be0
Andy Farthing <contact@andyfarthing.com>
2023-04-02 17:10:57 +0100

    Use socks ports instead of httptunnel

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,8 @@ i=0
 while [ $i -le "$NUMBER_OF_CONNECTIONS" ]; do
   PORT=$((STARING_PORT_NUMBER + i))
   echo "  server connection-$i 127.0.0.1:$PORT check inter 15s fall 1 rise 1" >> /etc/haproxy/haproxy.cfg
-  echo "HTTPTunnelPort 0.0.0.0:$PORT" >> /etc/tor/torrc
+  echo " SocksPort 0.0.0.0:$PORT" >> /etc/tor/torrc
   i=$(( i + 1 ))
 done
 
-haproxy -f /etc/haproxy/haproxy.cfg && tor -f /etc/tor/torrc
+haproxy -f /etc/haproxy/haproxy.cfg && privoxy privoxy.cfg && tor -f /etc/tor/torrc
